d84bc69d8769471b19b14c4b50ce56297989bf95
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-08-04 09:21:45 -0700

    before deploy hopefully

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-import rootReducer from '../reducers/index';
-
-export default (initialState) =>
-  createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
-  );
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
+}
